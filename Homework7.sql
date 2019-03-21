d8aaa757e4081c2adf17bd9d0e6f8ecb41b31b1f
sbogdan60 <sbogdan.sb@gmail.com>
2019-03-21 14:10:02 -0700

    save

--- Homework7.sql
+++ Homework7.sql
@@ -1,100 +1,146 @@
 use sakila;
 
 -- 1a
-select first_name, last_name from actor;
+SELECT 
+    first_name, last_name
+FROM
+    actor;
 
 -- 1b
-select concat(upper(first_name), " ", upper(last_name)) 
-	as "Actor Name"
-	from actor;
+SELECT 
+    CONCAT(UPPER(first_name), ' ', UPPER(last_name)) AS 'Actor Name'
+FROM
+    actor;
 
 -- 2a
-select actor_id, first_name, last_name 
-	from actor where lower(first_name) = "joe";
+SELECT 
+    actor_id, first_name, last_name
+FROM
+    actor
+WHERE
+    LOWER(first_name) = 'joe';
     
 -- 2b
-select first_name, last_name 
-	from actor
-    where lower(last_name) like "%gen%";
+SELECT 
+    first_name, last_name
+FROM
+    actor
+WHERE
+    LOWER(last_name) LIKE '%gen%';
     
 -- 2c
-select first_name, last_name 
-	from actor
-    where lower(last_name) like "%li%"
-    order by last_name, first_name;
+SELECT 
+    first_name, last_name
+FROM
+    actor
+WHERE
+    LOWER(last_name) LIKE '%li%'
+ORDER BY last_name , first_name;
     
 -- 2d
-select country_id, country 
-	from country
-    where country in ("Afghanistan", "Bangladesh", "China");
+SELECT 
+    country_id, country
+FROM
+    country
+WHERE
+    country IN ('Afghanistan' , 'Bangladesh', 'China');
     
 -- 3a
-alter table actor
-add column description blob;
+ALTER TABLE actor
+ADD COLUMN description BLOB;
 
 -- 3b
-alter table actor
-drop column description;
+ALTER TABLE actor
+DROP COLUMN description;
 
 -- 4a
-select last_name, count(*) 
-	from actor group by last_name;
+SELECT 
+    last_name, COUNT(*)
+FROM
+    actor
+GROUP BY last_name;
     
 -- 4b
-select last_name, count(*) 
-	from actor group by last_name having count(*) >= 2;
+SELECT 
+    last_name, COUNT(*)
+FROM
+    actor
+GROUP BY last_name
+HAVING COUNT(*) >= 2;
     
 -- 4c
-select first_name, last_name, actor_id from actor
-where lower(last_name) = 'williams';
 
--- Use ID found from last query: 172
 UPDATE actor 
-SET first_name = 'HARPO' 
-WHERE actor_id = '172';
+SET 
+    first_name = IF(first_name = 'GROUCHO',
+        'HARPO',
+        first_name)
+WHERE
+    last_name = 'WILLIAMS';
 
 -- 4d 
-UPDATE actor
-SET first_name = 
-IF ( first_name = 'HARPO', 'GROUCHO', first_name )
-WHERE actor_id = 172;
+UPDATE actor 
+SET 
+    first_name = IF(first_name = 'HARPO',
+        'GROUCHO',
+        first_name)
+WHERE
+    last_name = 'WILLIAMS';
 
 -- 5a
-show create table address;
+SHOW CREATE TABLE address;
 
 -- 6a
-select st.first_name, st.last_name, ad.address 
-from staff st
-left join address ad
-on st.address_id = ad.address_id;
+SELECT 
+    s.first_name, s.last_name, a.address
+FROM
+    staff s
+        LEFT JOIN
+    address a ON s.address_id = a.address_id;
 
 -- 6b
-select st.first_name, st.last_name,  sum(py.amount)
-from payment py 
-left join staff st
-on py.staff_id = st.staff_id
-WHERE payment_date >='2005-08-01 00:00:00'
-AND payment_date <'2005-08-31 00:00:00'
-group by st.staff_id;
+SELECT 
+    s.first_name, s.last_name, SUM(p.amount)
+FROM
+    payment p
+        LEFT JOIN
+    staff s ON p.staff_id = s.staff_id
+WHERE
+    payment_date >= '2005-08-01 00:00:00'
+        AND payment_date < '2005-08-31 00:00:00'
+GROUP BY s.staff_id;
 
 -- 6c
-select film.title, count(fa.actor_id)
-from film
-inner join film_actor fa on film.film_id = fa.film_id
-group by film.title;
+SELECT 
+    f.title, COUNT(fa.actor_id)
+FROM
+    film f
+        INNER JOIN
+    film_actor fa ON f.film_id = fa.film_id
+GROUP BY f.title;
 
 -- 6d
-select film.title, count(inventory.inventory_id) as 'Inventory Number'
-from film
-inner join inventory on film.film_id = inventory.film_id
-where lower(film.title) = 'hunchback impossible';
+SELECT 
+    f.title,
+    COUNT(i.inventory_id) AS 'Inventory Number'
+FROM
+    film f
+        INNER JOIN
+    inventory i ON f.film_id = i.film_id
+WHERE
+    LOWER(f.title) = 'hunchback impossible';
 
 -- 6e
-select cust.first_name, cust.last_name, sum(pay.amount) as 'Total Payments'
-from customer cust
-inner join payment pay on cust.customer_id = pay.customer_id
-group by cust.customer_id
-order by cust.last_name;
+SELECT 
+    c.first_name,
+    c.last_name,
+    SUM(p.amount) AS 'Total Payments'
+FROM
+    customer c
+        INNER JOIN
+    payment p ON c.customer_id = p.customer_id
+GROUP BY c.customer_id
+ORDER BY c.last_name;
 
 -- 7a 
 SELECT 
@@ -131,17 +177,17 @@ WHERE
                     
 -- 7c 
  SELECT 
-    c.first_name, c.last_name, c.email
+    cu.first_name, cu.last_name, cu.email
 FROM
-    customer c
+    customer cu
         INNER JOIN
-    address a ON c.address_id = a.address_id
+    address a ON cu.address_id = a.address_id
         INNER JOIN
-    city ON a.city_id = city.city_id
+    city ci ON a.city_id = ci.city_id
         INNER JOIN
-    country ON city.country_id = country.country_id
+    country co ON ci.country_id = co.country_id
 WHERE
-    LOWER(country.country) = 'canada';
+    LOWER(co.country) = 'canada';
  
  -- 7d
  SELECT 
@@ -150,4 +196,84 @@ FROM
     film
 WHERE
     rating IN ('G' , 'PG', 'PG-13');
+    
+-- 7e
+SELECT 
+    f.title, COUNT(r.inventory_id) as "Times Rented"
+FROM
+    film f
+        INNER JOIN
+    inventory i ON f.film_id = i.film_id
+        INNER JOIN
+    rental r ON i.inventory_id = r.inventory_id
+GROUP BY f.title
+ORDER BY COUNT(r.inventory_id) DESC;
  
+-- 7f
+SELECT 
+    st.store_id, SUM(p.amount) AS 'Total Sales'
+FROM
+    store st
+        INNER JOIN
+    staff sf ON st.store_id = sf.store_id
+        INNER JOIN
+    rental r ON sf.staff_id = r.staff_id
+        INNER JOIN
+    payment p ON r.rental_id = p.rental_id
+GROUP BY st.store_id;
+
+-- 7g
+SELECT 
+    s.store_id, ci.city, co.country
+FROM
+    store s
+        INNER JOIN
+    address a ON s.address_id = a.address_id
+        INNER JOIN
+    city ci ON a.city_id = ci.city_id
+        INNER JOIN
+    country co ON ci.country_id = co.country_id;
+
+-- 7h
+SELECT 
+    c.name AS 'Category', SUM(p.amount) AS 'Gross Sales'
+FROM
+    category c
+        INNER JOIN
+    film_category f ON c.category_id = f.category_id
+        INNER JOIN
+    inventory i ON f.film_id = i.film_id
+        INNER JOIN
+    rental r ON i.inventory_id = r.inventory_id
+        INNER JOIN
+    payment p ON r.rental_id = p.rental_id
+GROUP BY c.name
+ORDER BY SUM(p.amount) DESC
+LIMIT 5;
+
+-- 8a
+CREATE VIEW top_5 AS
+    SELECT 
+        c.name AS 'Category', SUM(p.amount) AS 'Gross Sales'
+    FROM
+        category c
+            INNER JOIN
+        film_category f ON c.category_id = f.category_id
+            INNER JOIN
+        inventory i ON f.film_id = i.film_id
+            INNER JOIN
+        rental r ON i.inventory_id = r.inventory_id
+            INNER JOIN
+        payment p ON r.rental_id = p.rental_id
+    GROUP BY c.name
+    ORDER BY SUM(p.amount) DESC
+    LIMIT 5;
+    
+-- 8b 
+SELECT 
+    *
+FROM
+    top_5;
+
+-- 8c
+DROP VIEW top_5;
